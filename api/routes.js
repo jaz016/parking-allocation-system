b78437b4cf61ab88bb7477b698fd2e17184037a5
Jasper Bautista <xjet16@gmail.com>
2023-05-18 21:08:50 +0800

    build /park endpoint (initial)

--- api/routes.js
+++ api/routes.js
@@ -15,7 +15,7 @@ router.post('/save-parking-lot', (req, res, next) => {
 	// console.log('entryPoints',entryPoints);
 	// console.log('parkingSlots',parkingSlots);
 
-	// todo: put this in a class
+	// todo: put this in a class, add models
 	jsonData.parkingLot = {
 		entryPoints,
 		parkingSlots: parkingSlots.map((slot,i) => {
@@ -26,9 +26,10 @@ router.post('/save-parking-lot', (req, res, next) => {
 	// clear parked cars
 	jsonData.parkData = [];
 
+	// todo: create util function for this
 	fs.writeFile(jsonPath, JSON.stringify(jsonData), (err) => {
 		if(err) {
-			console.log('error: ', err.message); // todo
+			console.log('error: ', err.message); // todo, detail error msg
 			return res.status(500).json({
 				data: [],
 				message: 'Something went wrong'
@@ -37,7 +38,7 @@ router.post('/save-parking-lot', (req, res, next) => {
 
 		fs.readFile(jsonPath, 'utf-8', (err, data) => {
 			if(err) {
-				console.log('error: ', err.message); // todo
+				console.log('error: ', err.message); // todo, detail error msg
 				return res.status(500).json({
 					data: [],
 					message: 'Something went wrong'
@@ -57,5 +58,50 @@ router.post('/save-parking-lot', (req, res, next) => {
 	// })
 
 });
+
+
+// todo: put it this a controller
+router.post('/park', (req, res, next) => {
+	const { carType } = req.body;
+
+	console.log('jsonData.cars',jsonData.cars);
+	const nextId = jsonData.cars.length > 0 ? Math.max(...jsonData.cars.map(car => car.carId)) + 1 : 1;
+	console.log('nextId', nextId);
+
+	const newCar = {
+		carId: nextId,
+		type: carType,
+		isParked: true
+	}
+
+	jsonData.cars.push(newCar); // todo: put this in a class (instantiate a car object), add models
+
+	// todo: create util function for this
+	fs.writeFile(jsonPath, JSON.stringify(jsonData), (err) => {
+		if(err) {
+			console.log('error: ', err.message); // todo, detail error msg
+			return res.status(500).json({
+				data: [],
+				message: 'Something went wrong'
+			})
+		}
+
+		fs.readFile(jsonPath, 'utf-8', (err, data) => {
+			if(err) {
+				console.log('error: ', err.message); // todo, detail error msg
+				return res.status(500).json({
+					data: [],
+					message: 'Something went wrong'
+				})
+			}
+
+			return res.status(200).json({
+				data: newCar,
+				message: 'Data saved successfully'
+			})
+		})
+	})
+})
+
 
 module.exports = router;
