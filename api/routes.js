944d8d5356367b31ef710cde997d86b672b65413
Jasper Bautista <xjet16@gmail.com>
2023-05-18 20:46:32 +0800

    build save-parking-lot endpoint

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,57 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
+const jsonPath = path.join(__dirname, 'data.json');
+const data = fs.readFileSync(jsonPath);
+const jsonData = JSON.parse(data);
 
-router.get('/test', (req, res, next) => {
-	return res.status(200).json({
-		data: 'test',
-		message: 'endpoint reached successfully'
+
+router.post('/save-parking-lot', (req, res, next) => {
+
+	const { entryPoints, parkingSlots } = req.body;
+
+	// console.log('entryPoints',entryPoints);
+	// console.log('parkingSlots',parkingSlots);
+
+	// todo: put this in a class
+	jsonData.parkingLot = {
+		entryPoints,
+		parkingSlots: parkingSlots.map((slot,i) => {
+			return {slotId: i+1, slotData: slot}
+		}),
+	}
+
+	fs.writeFile(jsonPath, JSON.stringify(jsonData), (err) => {
+		if(err) {
+			console.log('error: ', err.message); // todo
+			return res.status(500).json({
+				data: [],
+				message: 'Something went wrong'
+			})
+		}
+
+		fs.readFile(jsonPath, 'utf-8', (err, data) => {
+			if(err) {
+				console.log('error: ', err.message); // todo
+				return res.status(500).json({
+					data: [],
+					message: 'Something went wrong'
+				})
+			}
+
+			return res.status(200).json({
+				data: JSON.parse(data).parkingLot,
+				message: 'Data saved successfully'
+			})
+		})
 	})
+
+	// return res.status(200).json({
+	// 	data: 'test',
+	// 	message: 'success'
+	// })
+
 });
 
 module.exports = router;
